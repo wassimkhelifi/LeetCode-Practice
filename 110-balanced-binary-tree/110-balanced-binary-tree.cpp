0016610086f6a7cef7e7848d8ffa092445a34239
Wassim Khelifi <46505211+wassimkhelifi@users.noreply.github.com>
2022-06-28 02:32:15 -0700

    Time: 29 ms (18.28%), Space: 20.9 MB (54.26%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/110-balanced-binary-tree.cpp
@@ -9,39 +9,27 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-// class Solution {
-// public:
-//     bool isBalanced(TreeNode* root) {
-//         if (!root) return true;
-//         bool balanced = true;
-//         height(root, balanced);
-//         return balanced;
-//     }
-    
-//     bool height(TreeNode* node, bool& balanced) {
-//         if (!node) return 0;
-//         int leftSubtreeHeight = height(node -> left, balanced);
-//         int rightSubtreeHeight = height(node -> right, balanced);
-//         if (abs(leftSubtreeHeight - rightSubtreeHeight) > 1) balanced = false;
-//         return max(leftSubtreeHeight, rightSubtreeHeight) + 1;
-//     }
-// };
-
-
 class Solution {
 public:
     bool isBalanced(TreeNode* root) {
         if (!root) return true;
+        
         bool balanced = true;
         height(root, balanced);
+        
         return balanced;
     }
     
-    int height(TreeNode* root, bool& balanced) {
-        if (!root) return 0;
-        int leftHeight = height(root -> left, balanced);
-        int rightHeight = height(root -> right, balanced);
-        if (abs(leftHeight - rightHeight) > 1) balanced = false;
-        return max(leftHeight, rightHeight) + 1;
+    int height(TreeNode* node, bool& balanced) {
+        if (!node) return 0;
+        
+        int leftSubtreeHeight = height(node -> left, balanced);
+        int rightSubtreeHeight = height(node -> right, balanced);
+        
+        if (abs(leftSubtreeHeight - rightSubtreeHeight) > 1) {
+            balanced = false;
+        }
+        
+        return max(leftSubtreeHeight, rightSubtreeHeight) + 1;
     }
 };
