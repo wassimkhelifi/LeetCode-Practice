957b00ae0edcdb539f56fbb8479e1487d98170ae
Wassim Khelifi <46505211+wassimkhelifi@users.noreply.github.com>
2022-06-28 02:23:27 -0700

    Time: 285 ms (36.82%), Space: 62.8 MB (36.55%) - LeetHub

---
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+        vector<vector<int>> jobs;
+        for (int i = 0; i < startTime.size(); i++) {
+            jobs.push_back({endTime[i], startTime[i], profit[i]});
+        }
+        sort(jobs.begin(), jobs.end());
+        
+        map<int, int> endTimeToProfit;
+        endTimeToProfit[0] = 0;
+        for (auto& job : jobs) {
+            int possibleProfit = prev(endTimeToProfit.upper_bound(job[1])) -> second + job[2];
+            if (possibleProfit > endTimeToProfit.rbegin() -> second) {
+                endTimeToProfit[job[0]] = possibleProfit;
+            }
+        }
+        
+        return endTimeToProfit.rbegin() -> second;
+    }
+};
