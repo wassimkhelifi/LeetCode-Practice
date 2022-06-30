4c7dcad02ca4402b091989ef20ac06178404a5ff
Wassim Khelifi <46505211+wassimkhelifi@users.noreply.github.com>
2022-06-30 01:17:51 -0700

    Time: 0 ms (100.00%), Space: 8.1 MB (46.51%) - LeetHub

---
+++ 224-basic-calculator/224-basic-calculator.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int calculate(string s) {
+        int sign = 1;
+        int currentNumber = 0;
+        int result = 0;
+        stack<int> partialResults;
+        for (auto& character : s) {
+            if (isdigit(character)) currentNumber = currentNumber * 10 + (character - '0');
+            else if (character == '-' || character == '+') {
+                result += (currentNumber * sign);
+                if (character == '-') sign = -1;
+                else sign = 1;
+                currentNumber = 0;
+            } else if (character == '(') {
+                partialResults.push(result);
+                partialResults.push(sign);
+                result = 0;
+                sign = 1;
+                currentNumber = 0;
+            } else if (character == ')') {
+                result += (currentNumber * sign);
+                result *= partialResults.top(); partialResults.pop();
+                result += partialResults.top(); partialResults.pop();
+                sign = 1;
+                currentNumber = 0;
+            }
+        }
+        
+        return result + (currentNumber * sign);
+    }
+};
