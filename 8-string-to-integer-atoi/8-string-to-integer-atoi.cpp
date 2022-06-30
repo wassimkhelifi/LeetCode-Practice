a6464b1e6a15d0b10b04bc0589650060065afb4d
Wassim Khelifi <46505211+wassimkhelifi@users.noreply.github.com>
2022-06-30 02:06:41 -0700

    Time: 7 ms (39.72%), Space: 7.1 MB (52.10%) - LeetHub

---
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        if (s.empty()) return 0;
+        
+        int index = 0;
+        while (index < s.size() && isspace(s[index])) index++;
+        
+        bool isPositive = true;
+        if (index < s.size() && (s[index] == '+' || s[index] == '-')) {
+            if (s[index] == '-') isPositive = false;
+            index++;
+        }
+        
+        int integer = 0;
+        while (index < s.size() && isdigit(s[index])) {
+            int digit = s[index] - '0';
+            if (integer > INT_MAX / 10 || (integer == INT_MAX / 10 && digit > INT_MAX % 10)) {
+                if (isPositive) return INT_MAX;
+                return INT_MIN;
+            }
+            
+            integer = integer * 10 + digit;
+            index++;
+        }
+        
+        if (isPositive) return integer;
+        else return -integer;
+    }
+};
