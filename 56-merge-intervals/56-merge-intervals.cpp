32cd201ebaf4b6896807b0a4ca515dc3e4b5bb58
Wassim Khelifi <46505211+wassimkhelifi@users.noreply.github.com>
2022-07-01 02:21:04 -0700

    Time: 67 ms (36.56%), Space: 19.1 MB (46.14%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        
+        vector<vector<int>> mergedIntervals = {intervals.front()};
+        intervals.erase(intervals.begin());
+        
+        int index = 0;
+        for (auto& interval : intervals) {
+            vector<int>& mergedInterval = mergedIntervals[index];
+            if (interval.front() <= mergedInterval.back() && interval.back() >= mergedInterval.back()) {
+                mergedInterval = {mergedInterval.front(), interval.back()};
+            } else if (interval.front() >= mergedInterval.back() && interval.back() >= mergedInterval.back()) {
+                mergedIntervals.push_back(interval);
+                index++;
+            }
+        }
+        
+        return mergedIntervals;
+    }
+};
