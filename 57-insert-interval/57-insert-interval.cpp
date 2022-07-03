d25f15cb306bdc86ec5d6c819ef4ac49f6217e1a
Wassim Khelifi <46505211+wassimkhelifi@users.noreply.github.com>
2022-07-02 23:06:47 -0700

    Time: 42 ms (12.98%), Space: 17.2 MB (37.85%) - LeetHub

--- 57-insert-interval/57-insert-interval.cpp
+++ 57-insert-interval/57-insert-interval.cpp
@@ -3,16 +3,15 @@ public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         intervals.push_back(newInterval);
         sort(intervals.begin(), intervals.end());
-        
         vector<vector<int>> mergedIntervals = {intervals.front()};
         intervals.erase(intervals.begin());
         
         int index = 0;
         for (auto& interval : intervals) {
-            vector<int>& mergedInterval = mergedIntervals[index];
-            if (interval.front() <= mergedInterval.back() && interval.back() >= mergedInterval.back()) {
-                mergedInterval = {mergedInterval.front(), interval.back()};
-            } else if (interval.front() >= mergedInterval.back() && interval.back() >= mergedInterval.back()) {
+            vector<int>& topInterval = mergedIntervals[index]; 
+            if (interval.front() <= topInterval.back() && interval.back() >= topInterval.back()) {
+                topInterval = {topInterval.front(), interval.back()};
+            } else if (interval.front() >= topInterval.back() && interval.back() >= topInterval.back()) {
                 mergedIntervals.push_back(interval);
                 index++;
             }
