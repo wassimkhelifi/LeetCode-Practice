09ea346c98ca1ef9cdbb972281b90c69063a6a71
Wassim Khelifi <46505211+wassimkhelifi@users.noreply.github.com>
2022-07-05 01:39:25 -0700

    Time: 2 ms (41.92%), Space: 6.1 MB (30.85%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if (n <= 1) return n;
-        
         map<int, int> memo;
         memo[1] = 1;
         memo[2] = 2;
         for (int i = 3; i <= n; i++) {
             memo[i] = memo[i - 1] + memo[i - 2];
         }
-        
         return memo[n];
     }
 };
