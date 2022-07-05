77b334cfa77d8bcd9b3e16b019b5cb142769934a
Wassim Khelifi <46505211+wassimkhelifi@users.noreply.github.com>
2022-07-04 23:41:13 -0700

    Time: 17 ms (90.48%), Space: 26.4 MB (47.37%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -9,6 +9,8 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+/**
 class Solution {
 public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
@@ -27,6 +29,31 @@ public:
         TreeNode* root = new TreeNode(value);
         root -> left = generateTree(preorder, left, inorderIndex[value] - 1, preorderIndex, inorderIndex);
         root -> right = generateTree(preorder, inorderIndex[value] + 1, right, preorderIndex, inorderIndex);
+        return root;
+    }
+};
+*/
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        map<int, int> inorderToIndex;
+        for (int i = 0; i < inorder.size(); i++) {
+            inorderToIndex[inorder[i]] = i;
+        }
+        
+        int preorderIndex = 0;
+        return generateTree(preorder, 0, inorder.size() - 1, preorderIndex, inorderToIndex);
+    }
+    
+    TreeNode* generateTree(vector<int>& preorder, int left, int right, int& preorderIndex, map<int, int>& inorderToIndex) {
+        if (left > right) {
+            return NULL;
+        }
+        
+        int value = preorder[preorderIndex++];
+        TreeNode* root = new TreeNode(value);
+        root -> left = generateTree(preorder, left, inorderToIndex[value] - 1, preorderIndex, inorderToIndex);
+        root -> right = generateTree(preorder, inorderToIndex[value] + 1, right, preorderIndex, inorderToIndex);
         return root;
     }
 };
