54ec7ea8b3ad26a1fe9649cd7966278081897bb4
Wassim Khelifi <46505211+wassimkhelifi@users.noreply.github.com>
2022-07-05 01:13:09 -0700

    Time: 573 ms (24.15%), Space: 47.5 MB (26.76%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -5,25 +5,25 @@ public:
         if (size == 1) return s;
         
         vector<vector<bool>> memo(size, vector<bool>(size, false));
-        for (int i = 0; i < size; i++) memo[i][i] = true;
+        for (int i = 0; i < size; i++) {
+            memo[i][i] = true;
+        }
         
-        string longestSubstring;
-        longestSubstring.push_back(s.front());
-        int length = 1;
-        for (int right = 1; right < size; right++) {
+        string longest;
+        longest += s.front();
+        for (int right = 1; right < s.size(); right++) {
             for (int left = right - 1; left >= 0; left--) {
                 if (s[left] == s[right]) {
-                    if (right - left == 1 || memo[left + 1][right - 1]) {
+                    if (memo[left + 1][right - 1] or right - left == 1) {
                         memo[left][right] = true;
-                        if (right - left + 1 > length) {
-                            length = right - left + 1;
-                            longestSubstring = s.substr(left, right - left + 1);
+                        if (right - left + 1 > longest.size()) {
+                            longest = s.substr(left, right - left + 1);
                         }
                     }
                 }
             }
         }
         
-        return longestSubstring;
+        return longest;
     }
 };
