4c3456c1b1bedfa62e8e02c76f65a30cba52feed
Wassim Khelifi <46505211+wassimkhelifi@users.noreply.github.com>
2022-07-05 01:37:19 -0700

    Time: 4 ms (28.07%), Space: 6.5 MB (25.63%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> memo(m, vector<int>(n, 0));
-        for (int i = 0; i < m; i++) memo[i][0] = 1;
-        for (int i = 0; i < n; i++) memo[0][i] = 1;
-        
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                memo[i][j] = memo[i - 1][j] + memo[i][j - 1];
+        vector<vector<int>> memo(m, vector<int>(n));
+        for (int col = 0; col < n; col++) {
+            memo[0][col] = 1;
+        }
+        for (int row = 0; row < m; row++) {
+            memo[row][0] = 1;
+        }
+        for (int row = 1; row < m; row++) {
+            for (int col = 1; col < n; col++) {
+                memo[row][col] = memo[row - 1][col] + memo[row][col - 1];
             }
         }
         
