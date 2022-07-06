470a3bd1df1a9b3ad2ee26a73ce4563543c988e2
Wassim Khelifi <46505211+wassimkhelifi@users.noreply.github.com>
2022-07-05 22:19:42 -0700

    Time: 7 ms (94.74%), Space: 14 MB (48.99%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -4,18 +4,19 @@ public:
         if (image[sr][sc] != color) {
             dfs(image, sr, sc, image[sr][sc], color);
         }
+        
         return image;
     }
     
     void dfs(vector<vector<int>>& grid, int row, int col, int oldColor, int newColor) {
-        if (row < 0 || row >= grid.size() || col < 0 || col >= grid[0].size() || grid[row][col] != oldColor) {
+        if (row < 0 or row >= grid.size() or col < 0 or col >= grid[0].size() or grid[row][col] != oldColor) {
             return;
         }
         
         grid[row][col] = newColor;
-        dfs(grid, row + 1, col, oldColor, newColor);
         dfs(grid, row - 1, col, oldColor, newColor);
-        dfs(grid, row, col + 1, oldColor, newColor);
+        dfs(grid, row + 1, col, oldColor, newColor);
         dfs(grid, row, col - 1, oldColor, newColor);
+        dfs(grid, row, col + 1, oldColor, newColor);
     }
 };
